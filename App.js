312312c2dc81c01ffb637ab90b2191682bca856e
resync <support@readable.so>
2024-02-06 17:36:27 +0530

    resync auto-sync

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { useRef, useState } from 'react';
 import { View, StyleSheet, Animated, PanResponder, Dimensions } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import SwipeAnimationApp from './SwipeAnimationApp';
+
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SCREEN_HEIGHT = Dimensions.get('window').height;
