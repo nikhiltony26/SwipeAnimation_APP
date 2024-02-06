291e7f4ef9b29fb0ede84a21390f7aced105b307
resync <support@readable.so>
2024-02-06 19:44:09 +0530

    resync auto-sync

--- App.js
+++ App.js
@@ -87,12 +87,12 @@ const Card = ({ backgroundColor, index, onSwipe }) => {
       >
         <Animated.View style={[styles.iconContainer, { opacity: leftIconOpacity }]}>
           <View style={[styles.iconBackground, { backgroundColor: 'red' }]}>
-            <Icon name="times" size={30} color="white" />
+            <Icon name="times" size={40} color="white" />
           </View>
         </Animated.View>
         <Animated.View style={[styles.iconContainer, { opacity: rightIconOpacity, left: 120 }]}>
           <View style={[styles.iconBackground, { backgroundColor: 'green' }]}>
-            <Icon name="check" size={30} color="white" />
+            <Icon name="check" size={40} color="white" />
           </View>
         </Animated.View>
       </Animated.View>
