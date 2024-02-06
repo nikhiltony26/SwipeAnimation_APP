0f7c4006d393420b8c2064d2acbe26a2107691d1
resync <support@readable.so>
2024-02-06 19:35:13 +0530

    resync auto-sync

--- App.js
+++ App.js
@@ -35,41 +35,17 @@ const Card = ({ backgroundColor, index, onSwipe }) => {
       onStartShouldSetPanResponder: () => true,
       onPanResponderMove: (event, gesture) => {
         position.setValue({ x: gesture.dx, y: 0 });
-       // Adjusting icon opacities based on gesture movement
         if (gesture.dx > 50) {
-          Animated.timing(rightIconOpacity, {
-            duration: 0,
-            useNativeDriver: true,
-          }).start();
-          Animated.timing(leftIconOpacity, {
-            toValue: 0,
-            duration: 0,
-            useNativeDriver: false,
-          }).start();
+          rightIconOpacity.setValue(1);
+          leftIconOpacity.setValue(0);
         } else if (gesture.dx < -50) {
-          Animated.timing(rightIconOpacity, {
-            toValue: 0,
-            duration: 0,
-            useNativeDriver: false,
-          }).start();
-          Animated.timing(leftIconOpacity, {
-            toValue: 1,
-            duration: 0,
-            useNativeDriver: false,
-          }).start();
+          rightIconOpacity.setValue(0);
+          leftIconOpacity.setValue(1);
         } else {
-          Animated.timing(rightIconOpacity, {
-            toValue: 0,
-            duration: 0,
-            useNativeDriver: false,
-          }).start();
-          Animated.timing(leftIconOpacity, {
-            toValue: 0,
-            duration: 0,
-            useNativeDriver: false,
-          }).start();
+          rightIconOpacity.setValue(0);
+          leftIconOpacity.setValue(0);
         }
-      },  
+      },
       
       onPanResponderRelease: (event, gesture) => {
         if (gesture.dx > 120) {
@@ -119,7 +95,7 @@ const Card = ({ backgroundColor, index, onSwipe }) => {
         <Animated.View style={[styles.iconContainer, { opacity: leftIconOpacity,top: -120 }]}>
       <Icon name="times" size={60} color="rgba(255, 0, 0, 1.0)" backgroundColor='rgba(255, 0, 0, 0.7)'/>
         </Animated.View>
-        <Animated.View style={[styles.iconContainer, { opacity: rightIconOpacity, left: 10 }]}>
+        <Animated.View style={[styles.iconContainer, { opacity: rightIconOpacity, left: 30 }]}>
           <Icon name="check" size={60} color="rgba(0, 255, 0, 1.0)" />
         </Animated.View>
       </Animated.View>
@@ -172,6 +148,7 @@ const styles = StyleSheet.create({
   },
   iconBackground: {
    borderRadius: 100,
+
     padding: 10,
   },
 });
