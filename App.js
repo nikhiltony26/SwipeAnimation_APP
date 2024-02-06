8f34a38b36460d260077230bd62f0b0bfe1156c1
resync <support@readable.so>
2024-02-06 20:22:52 +0530

    resync auto-sync

--- App.js
+++ App.js
@@ -141,7 +141,7 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
-    elevation: 5,
+    elevation: 100,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'white',
