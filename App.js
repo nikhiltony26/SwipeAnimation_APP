efebe82625f4fd2e0f2dfc5c695caa1755c6eb12
resync <support@readable.so>
2024-02-06 19:44:50 +0530

    resync auto-sync

--- App.js
+++ App.js
@@ -143,7 +143,7 @@ const styles = StyleSheet.create({
     top: -150,
   },
   iconBackground: {
-    borderRadius: 60, // Ensures circular shape
+    borderRadius: 10, // Ensures circular shape
     padding: 10,
   },
 });
