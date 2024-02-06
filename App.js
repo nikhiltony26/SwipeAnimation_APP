92115a50793b21200d9e7bdaaa3d2e2ac668ba4f
resync <support@readable.so>
2024-02-06 20:25:05 +0530

    resync auto-sync

--- App.js
+++ App.js
@@ -133,7 +133,7 @@ const styles = StyleSheet.create({
   card: {
     width: CARD_WIDTH,
     height: CARD_HEIGHT,
-    borderRadius: 10,
+    borderRadius: 1,
     shadowColor: '#000',
     shadowOffset: {
       width: 0,
